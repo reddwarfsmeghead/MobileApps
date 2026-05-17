8b31eaf4dc7d6b98765dfd21d6e2a3dd18e92567
reddwarfsmeghead <stephen.lindsay.irving@gmail.com>
2026-05-17 15:25:24 +1000

    Update from Bolt.diy

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 import './style.css'
+import { setupHomeApp } from './src/homeApp.js'
 import { setupDiceRNGApp } from './src/diceRNGApp.js'
 
 const app = document.querySelector('#app')
-setupDiceRNGApp(app)
+
+// Simple SPA navigation state
+let currentPage = 'home';
+
+function render() {
+  if (currentPage === 'home') {
+    setupHomeApp(app, () => {
+      currentPage = 'dice';
+      render();
+    });
+  } else if (currentPage === 'dice') {
+    setupDiceRNGApp(app);
+  }
+}
+
+// Start at home page
+render();
